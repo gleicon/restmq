ecabd98f5bf2a366a437c40f524d9b3f8ecf867b
Alexandre Fiori <fiorix@gmail.com>
2009-12-15 16:55:31 -0200

    comet client based on twisted's downloadPage

---
+++ examples/test_comet.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import simplejson
+from twisted.web import client
+from twisted.python import log
+from twisted.internet import reactor
+
+class CometClient(object):
+    def write(self, content):
+        try:
+            data = simplejson.loads(content)
+        except Exception, e:
+            log.err("cannot decode json: %s" % str(e))
+            log.err("json is: %s" % content)
+        else:
+            log.msg("got data: %s" % repr(data))
+
+    def close(self):
+        pass
+
+if __name__ == "__main__":
+    log.startLogging(sys.stdout)
+    client.downloadPage("http://localhost:8888/c/test", CometClient())
+    reactor.run()
